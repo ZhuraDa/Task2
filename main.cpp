49e8d4cc29953b69a8650a18e84cf5edce7681fe
z3lph1x <72938782+z3lph1x@users.noreply.github.com>
2022-03-27 23:01:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,32 @@
 
 using namespace std;
 
+
 double
-KIR(double Wl_1n1, double Wln1, double Wl__1n1, double Wl_1n2, double Wln2, double Wl__1n2, double Wl_1n3, double Wln3,
-    double Wl__1n3, double tau, double h, double A1, double A2, double A3, double H1,
-    double H2, double H3) {
+KIR1(double Wl_1n1, double Wln1, double Wl__1n1, double Wl_1n2, double Wln2, double Wl__1n2, double Wl_1n3, double Wln3,
+     double Wl__1n3, double tau, double h, double A1, double A2, double A3, double H1,
+     double H2, double H3) {
     return Wln1 - tau / (2 * h) * (A1 * (Wl__1n1 - Wl_1n1) + A2 * (Wl__1n2 - Wl_1n2) + A3 * (Wl__1n3 - Wl_1n3)) +
-           tau / (2 * h) * (H1 * (Wl__1n1 - 2 * Wln1 + Wl__1n1) + H2 * (Wl__1n2 - 2 * Wln2 + Wl__1n2) +
-                            H3 * (Wl__1n3 - 2 * Wln3 + Wl__1n3));
+           tau / (2 * h) * (H1 * (Wl__1n1 - 2 * Wln1 + Wl_1n1) + H2 * (Wl__1n2 - 2 * Wln2 + Wl_1n2) +
+                            H3 * (Wl__1n3 - 2 * Wln3 + Wl_1n3));
+}
+
+double
+KIR2(double Wl_1n1, double Wln1, double Wl__1n1, double Wl_1n2, double Wln2, double Wl__1n2, double Wl_1n3, double Wln3,
+     double Wl__1n3, double tau, double h, double A1, double A2, double A3, double H1,
+     double H2, double H3) {
+    return Wln2 - tau / (2 * h) * (A1 * (Wl__1n1 - Wl_1n1) + A2 * (Wl__1n2 - Wl_1n2) + A3 * (Wl__1n3 - Wl_1n3)) +
+           tau / (2 * h) * (H1 * (Wl__1n1 - 2 * Wln1 + Wl_1n1) + H2 * (Wl__1n2 - 2 * Wln2 + Wl_1n2) +
+                            H3 * (Wl__1n3 - 2 * Wln3 + Wl_1n3));
+}
+
+double
+KIR3(double Wl_1n1, double Wln1, double Wl__1n1, double Wl_1n2, double Wln2, double Wl__1n2, double Wl_1n3, double Wln3,
+     double Wl__1n3, double tau, double h, double A1, double A2, double A3, double H1,
+     double H2, double H3) {
+    return Wln3 - tau / (2 * h) * (A1 * (Wl__1n1 - Wl_1n1) + A2 * (Wl__1n2 - Wl_1n2) + A3 * (Wl__1n3 - Wl_1n3)) +
+           tau / (2 * h) * (H1 * (Wl__1n1 - 2 * Wln1 + Wl_1n1) + H2 * (Wl__1n2 - 2 * Wln2 + Wl_1n2) +
+                            H3 * (Wl__1n3 - 2 * Wln3 + Wl_1n3));
 }
 
 
@@ -19,10 +38,9 @@ int main() {
     double L = 20.;
     double h = L / 100.;
     int Nx = 100;
-    double tau = 10e-7;
-    int Nt = 200001;
+    double tau = 0.000001;
     double g = (5. / 3.);
-    double v;
+    double Co = 0.005;
     vector<vector<double>> w1;
     w1.assign(2, vector<double>(Nx));// ro
     vector<vector<double>> w2;
@@ -48,43 +66,50 @@ int main() {
     A[0][1] = 1.;
     A[0][2] = 0.;
     A[1][2] = g - 1.;
+    double e;
+    double c;
+    double u;
+    double t = 0;
+    double T = 0.02;
 
     //<<
-    for (int t = 0; t < Nt; t++) {
+    t += tau;
+    while (t <= T) {
         for (int i = 0; i < Nx; i++) {
             A[1][0] = -(w2[0][i] / w1[0][i]) * (w2[0][i] / w1[0][i]);
             A[1][1] = w2[0][i] / w1[0][i] * 2.;
             A[2][0] = -g * (w2[0][i] / w1[0][i]) * (w3[0][i] / w1[0][i]);
             A[2][1] = g * (w3[0][i] / w1[0][i]);
             A[2][2] = w2[0][i] / w1[0][i];
-            double e = w3[0][i] / w1[0][i];
-            double c = g * (g - 1) * e;
-            double u = A[2][2];
-            double a1 = abs(u + c);
-            double a_1 = abs(u - c);
-            v = a1;
+            e = w3[0][i] / w1[0][i];
+            c = sqrt(g * (g - 1) * e);
+            u = A[2][2];
+            double a_1 = abs(u + c);
+            double a1 = abs(u - c);
             double a = abs(u);
             //lutiy pizdec, multiplied matrix before last part of equation, looks like shit and i should remake this
-            H[0][0] = -u * a1 / (2. * c) + a + a_1 * u / (2. * c);
-            H[0][1] = (a1 - a_1) / (2. * c);
-            H[0][2] = (g - 1.) * (a1 / (2. * c * c) - a / (c * c) + a_1 / (2. * c * c));
-            H[1][0] = -a1 * (u + c) * u / (2. * c) + u * a + a_1 * (u - c) * u / (2. * c);
-            H[1][1] = a1 * (u + c) / (2. * c) - a_1 * (u - c) / (2. * c);
-            H[1][2] = (g - 1.) *
-                      (a1 * (u + c) / (2. * c * c) - u * a / (c * c) + a_1 * (u - c) / (2. * c * c));
-            H[2][0] = (a1 - a_1) * u * c / (2. * (g - 1.));
-            H[2][1] = (a1 - a_1) * c / (2. * (g - 1.));
-            H[2][2] = (a1 + a_1) / 2.;
+            H[0][0] = -u * a_1 / (2. * c) + a + a1 * u / (2. * c);
+            H[0][1] = (a_1 - a1) / (2. * c);
+            H[0][2] = (g - 1.) * (a_1 / (2. * c * c) - a / (c * c) + a1 / (2. * c * c));
+            H[1][0] = -a_1 * (u + c) * u / (2. * c) + u * a + a1 * (u - c) * u / (2. * c);
+            H[1][1] = a_1 * (u + c) / (2. * c) - a1 * (u - c) / (2. * c);
+            H[1][2] = (g - 1) * (c * (a_1 - a1) + u * (a_1 + a1) - 2 * u * a) / (2 * c * c);
+            H[2][0] = (a_1 - a1) * u * c / (2. * (g - 1.));
+            H[2][1] = (a_1 - a1) * c / (2. * (g - 1.));
+            H[2][2] = (a_1 + a1) / 2.;
             if ((i != 0) and i != (Nx - 1)) {
-                w1[1][i] = KIR(w1[0][i - 1], w1[0][i], w1[0][i + 1], w2[0][i - 1], w2[0][i], w2[0][i + 1], w3[0][i - 1],
-                               w3[0][i], w3[0][i + 1], tau, h, A[0][0], A[0][1], A[0][2], H[0][0],
-                               H[0][1], H[0][2]);
-                w2[1][i] = KIR(w1[0][i - 1], w1[0][i], w1[0][i + 1], w2[0][i - 1], w2[0][i], w2[0][i + 1], w3[0][i - 1],
-                               w3[0][i], w3[0][i + 1], tau, h, A[1][0], A[1][1], A[1][2], H[1][0],
-                               H[1][1], H[1][2]);
-                w3[1][i] = KIR(w1[0][i - 1], w1[0][i], w1[0][i + 1], w2[0][i - 1], w2[0][i], w2[0][i + 1], w3[0][i - 1],
-                               w3[0][i], w3[0][i + 1], tau, h, A[2][0], A[2][1], A[2][2], H[2][0],
-                               H[2][1], H[2][2]);
+                w1[1][i] = KIR1(w1[0][i - 1], w1[0][i], w1[0][i + 1], w2[0][i - 1], w2[0][i], w2[0][i + 1],
+                                w3[0][i - 1],
+                                w3[0][i], w3[0][i + 1], tau, h, A[0][0], A[0][1], A[0][2], H[0][0],
+                                H[0][1], H[0][2]);
+                w2[1][i] = KIR2(w1[0][i - 1], w1[0][i], w1[0][i + 1], w2[0][i - 1], w2[0][i], w2[0][i + 1],
+                                w3[0][i - 1],
+                                w3[0][i], w3[0][i + 1], tau, h, A[1][0], A[1][1], A[1][2], H[1][0],
+                                H[1][1], H[1][2]);
+                w3[1][i] = KIR3(w1[0][i - 1], w1[0][i], w1[0][i + 1], w2[0][i - 1], w2[0][i], w2[0][i + 1],
+                                w3[0][i - 1],
+                                w3[0][i], w3[0][i + 1], tau, h, A[2][0], A[2][1], A[2][2], H[2][0],
+                                H[2][1], H[2][2]);
             }
         }
         //на границах
@@ -100,10 +125,9 @@ int main() {
             w2[0][i] = w2[1][i];
             w3[0][i] = w3[1][i];
         }
-       /* if(v*tau/h >0.01){
-            tau = h/v;
-            Nt = 0.02/tau;
-        }*/
+        tau = Co * h / max(max(abs(u + c), abs(u - c)), abs(u));
+        t += tau;
+        cout << t << endl;
     }
     //непосредственно вывод
     vector<double> P;
@@ -111,20 +135,20 @@ int main() {
     vector<double> Ro;
     Ro.resize(Nx);
     vector<double> U;
-    U.
-            resize(Nx);
+    U.resize(Nx);
     vector<double> E;
-    E.
-            resize(Nx);
+    E.resize(Nx);
     for (int i = 0; i < Nx; i++) {
         P[i] = (g - 1) * w3[0][i];
         Ro[i] = w1[1][i];
         U[i] = w2[1][i] / w1[1][i];
         E[i] = w3[1][i] / w1[1][i];
     }
-    ofstream out("P.txt");
+    ofstream out("U.txt");
     for (int x = 0; x < Nx; x++) {
-        out << P[x] << ' ';
+        out << U[x] << ' ';
     }
 }
+
+
 
